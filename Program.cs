383496e29acd3aa4eaca3c9c59e577a84098dcc2
SHADI JAM <shadijam@SHADIs-MacBook-Pro.local>
2016-10-24 09:05:22 -0500

    create file

--- Program.cs
+++ Program.cs
@@ -26,15 +26,11 @@ public class Program
         
         string term = Console.ReadLine();
         string key = "49rhltbtvSmshKivQd2EvXliQ0Zrp1vCsZ5jsnVtER81HAflRg";
-        IJSONAPI mashUrl = new MashapeAPI();
+        IJSONAPI mashapi = new MashapeAPI();
         
+        Nutrition n = await mashapi.GetData<Nutrition>("cheese", key);
+        Console.WriteLine(mashapi.ToJSON(n));
         
-        Nutrition data = await mashUrl.GetData<Nutrition>(term, key);
-        string result = data.nutrition.ToString();
-        Console.WriteLine();
-        // Nutrition n = await mashapi.GetData<Nutrition>("cheddar cheese", "APIKEY");
-        // Console.WriteLine(mashapi.ToJSON(n));  Gaucho: get JSON should work across
-        //all child classes
 
         // IJSONAPI googapi = new GoogleAPI();   ea. API will either have a header info(use) Postman
                                                                         //if header required.
